bf1d85b2333dcc0df928279ee9ebb0fb6606bfcd
Morteza Layegh <Morteza.Layegh-Mirhosseini@city.ac.uk>
2024-02-22 14:40:48 +0000

    Data Ingestion

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from src.mlProject import logger 
-logger.info("This is an info message11111111111")
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = 'data ingestion training pipeline'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
